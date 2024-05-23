5740f4ce453d6f34fde6938752360b39bde44323
Demilade362 <ademolademilade362@gmail.com>
2024-05-23 16:42:40 +0000

    conflict fixed in the store

--- Frontend/src/app/store.js
+++ Frontend/src/app/store.js
@@ -1,17 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
-<<<<<<< HEAD
-import userReducer, { userSlice } from '../features/User/user';
-
-
-export const store = configureStore({
-  reducer: {
-    user: userReducer 
-=======
 import userReducer from '../features/User/user';
+
 
 export const store = configureStore({
   reducer: {
     user: userReducer
->>>>>>> 595cfb0 (login requests)
-  },
-})
+  }
+});
